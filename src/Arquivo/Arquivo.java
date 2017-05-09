a31ee63ba749348dd4684be1a1bbdc544a43b7e9
Davi Wesley <davi.wesley@icloud.com>
2017-05-09 18:41:19 -0300

    escrita e leitura de arquvos
    ler um arquivo e carrega seus dados em uma lista encadeada

--- src/Arquivo/Arquivo.java
+++ src/Arquivo/Arquivo.java
@@ -16,25 +16,32 @@ import java.nio.file.Paths;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import com.sun.org.apache.xerces.internal.util.SynchronizedSymbolTable;
+
 import ListaEncadeada.ListaEncadeada;
 
 public class Arquivo {
 
-	public void leitura(String arquivo, ListaEncadeada l) throws IOException{
+	public void leitura(String arquivo) throws IOException {
 		int totaldelinhas = this.contaLinhas(arquivo);
+		ListaEncadeada l[] = new ListaEncadeada[totaldelinhas];
+		int vetor = 0;
 		Charset charset = Charset.forName("US-ASCII");
 		try (BufferedReader reader = Files.newBufferedReader(Paths.get(arquivo), charset)) {
 			String line = null;
+
 			while ((line = reader.readLine()) != null) {
 				String[] c2 = line.split(" ");
+				l[vetor] = new ListaEncadeada();
 				for (int i = 0; i < c2.length; i++) {
-					//System.out.print(c2[i]);
-					l.adicionarFim(Integer.parseInt(c2[i]));
-
+					l[vetor].adicionarFim(Integer.parseInt(c2[i]));
 				}
 				System.out.println("");
-				l.selectionSort();
-				l.imprimir();			
+				
+				l[vetor].selectionSort();
+				//l[vetor].imprimir();
+				this.escrever(l[vetor].toString());
+				vetor++;
 			}
 		} catch (IOException x) {
 			System.err.format("IOException: %s%n", x);
@@ -42,45 +49,35 @@ public class Arquivo {
 	}
 
 	public int contaLinhas(String filename) throws IOException {
-	    InputStream is = new BufferedInputStream(new FileInputStream(filename));
-	    try {
-	        byte[] c = new byte[1024];
-	        int count = 0;
-	        int readChars = 0;
-	        boolean endsWithoutNewLine = false;
-	        while ((readChars = is.read(c)) != -1) {
-	            for (int i = 0; i < readChars; ++i) {
-	                if (c[i] == '\n')
-	                    ++count;
-	            }
-	            endsWithoutNewLine = (c[readChars - 1] != '\n');
-	        }
-	        if(endsWithoutNewLine) {
-	            ++count;
-	        } 
-	        return count;
-	    } finally {
-	        is.close();
-	    }
-	}
-	
-	public void escrever() {
-
-		String path = "testeEscrita.txt";
-
+		InputStream is = new BufferedInputStream(new FileInputStream(filename));
 		try {
-
-			BufferedWriter buffWrite = new BufferedWriter(new FileWriter(path));
-
-			String linha = "ARQUIVO E MUITO FACILLLLL :)";
-			buffWrite.append(linha + "\n");
-			buffWrite.close();
-
-		} catch (IOException ex) {
-			Logger.getLogger(Arquivo.class.getName()).log(Level.SEVERE, null, ex);
+			byte[] c = new byte[1024];
+			int count = 0;
+			int readChars = 0;
+			boolean endsWithoutNewLine = false;
+			while ((readChars = is.read(c)) != -1) {
+				for (int i = 0; i < readChars; ++i) {
+					if (c[i] == '\n')
+						++count;
+				}
+				endsWithoutNewLine = (c[readChars - 1] != '\n');
+			}
+			if (endsWithoutNewLine) {
+				++count;
+			}
+			return count;
+		} finally {
+			is.close();
 		}
 	}
 
-
-
+	public void escrever(String s ) throws IOException{
+		FileWriter fw = null;
+		fw = new FileWriter("saida.txt",true);
+		BufferedWriter bw = new BufferedWriter(fw);
+		bw.write(s);
+		bw.newLine();
+		bw.close();
+		fw.close();		
+	}
 }
