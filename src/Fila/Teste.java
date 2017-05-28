eb3621c0bee6b144a01a57a9f730d1ae61ee7d46
daviwesley <wesley.sud7@gmail.com>
2017-05-28 15:28:59 -0300

    Teste adicionado

---
+++ src/Fila/Teste.java
@@ -0,0 +1,16 @@
+package Fila;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Fila f = new Fila();
+		f.adicionar(1);
+		f.adicionar(2);
+		f.adicionar(3);
+		f.remover();
+		System.out.println(f);
+
+	}
+
+}
