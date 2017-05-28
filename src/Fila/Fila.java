b664e55304de020e223c96b80a36438f48f09c31
daviwesley <wesley.sud7@gmail.com>
2017-05-28 15:18:19 -0300

    updated

--- src/Fila/Fila.java
+++ src/Fila/Fila.java
@@ -1,19 +1,52 @@
 package Fila;
 
-public class Fila {
+import ListaEncadeada.No;
+
+public class Fila  {
 	private int tam;
+	private No inicio;
+	private No fim;
 
-	public void adicionar(){
-
+	public void adicionar(int x){
+		//adiciona no fim
+		if(this.inicio == null){
+			this.adicionarInicio(x);
+		}else{
+		No n = new No();
+		n.setConteudo(x);
+		fim.setProx(n);
+		fim = n;
+		this.tam++;
+		}
 	}
-
-	public void remover(){
-
+	
+	private void adicionarInicio(int x){
+		No n = new No();
+		n.setConteudo(x);
+		if(inicio == null){
+			inicio = n;
+			fim = n;			
+		}else{
+			n.setProx(inicio);
+			this.inicio = n;				
+		}		
+		this.tam++;
 	}
-
+	
+	public void removerInicio(){
+		if(inicio == null){
+			throw new IllegalArgumentException("A lista está vazia");
+		}
+		No temp = inicio;
+		inicio = temp.getProx();
+		this.tam--;
+	}
+	
 	public void imprimir(){
-
+		for(No i = inicio; inicio != null; i = i.getProx()){
+			System.out.print(i.getConteudo() + " ");
+		}
 	}
-
+	
 	
 }
